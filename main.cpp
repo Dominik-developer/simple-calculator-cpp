0fa1e307b6a00daf337cd8c56763f64a75576d18
Dominik Szczepański <163560221+Dominik-developer@users.noreply.github.com>
2024-08-31 23:43:33 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int choice;
+
+    cout<<"Simple calculator in C++"<< endl;
+    cout<<"------------------------------"<<endl;
+
+    cout<<"What do you want today?"<<endl:
+    
+
+    cin>>choice;
+
+
+    return 0;
+}
